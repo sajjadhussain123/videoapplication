f1ffcc53c2f803491a8cce4734b9ee5528b47c1d
amirsaleemse <amirsaleem.se@gmail.com>
2017-05-05 21:38:49 +0500

    Update example.php

--- example.php
+++ example.php
@@ -2,5 +2,5 @@
 <?php
 // see README.md for an explanation of these config params
 $TWILIO_ACCOUNT_SID = 'ACe73857b390f50f18e129bd06e5f8796d';
- $TWILIO_API_KEY = 'SK4312f2642131cb3e5fe14d10651c730f';
- $TWILIO_API_SECRET = 'piwYtiAafXdHdGfccFSxDcYxxnHNw5vg';
+ $TWILIO_API_KEY = 'SK051a61daff9a9d9598af9ec93dce54e2';
+ $TWILIO_API_SECRET = 'hW7ff0MOFaFTlRKC3A4mNH5H8j9CGF4s';
