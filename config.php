d39d8479e0c808e67a3aee02c7dde5625233172c
amirsaleemse <amirsaleem.se@gmail.com>
2017-05-06 15:00:17 +0500

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 <?php
 // see README.md for an explanation of these config params
 $TWILIO_ACCOUNT_SID = 'ACe73857b390f50f18e129bd06e5f8796d';
- $TWILIO_API_KEY = 'SK051a61daff9a9d9598af9ec93dce54e2';
- $TWILIO_API_SECRET = 'hW7ff0MOFaFTlRKC3A4mNH5H8j9CGF4s';
+ $TWILIO_API_KEY = 'SK08e9355f5cba86f94992d45a68c1abd0';
+ $TWILIO_API_SECRET = 'dHCfmKfDTOfcRv8LzS8tJmu2IgMTu5oO';
