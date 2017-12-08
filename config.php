30dd996224648e6a4d5da9a3c5a9c49a389d5a49
Umair1199 <33391745+Umair1199@users.noreply.github.com>
2017-12-09 00:13:35 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 
 <?php
 // see README.md for an explanation of these config params
-$TWILIO_ACCOUNT_SID = 'AC849dcf6a39b53d68a0bfbb33648e574d';
- $TWILIO_API_KEY = 'SK4736e3fcd8d4b8f3850a27081d7e1a05';
- $TWILIO_API_SECRET = 'WGDwd88md1PUKykzGe3XJUvmxTjv4UiP';
+$TWILIO_ACCOUNT_SID = 'AC2851e10b057f0fd64db3aa6c0036494a';
+ $TWILIO_API_KEY = 'SK7c16abac5caf7534c0f61ef089a782e2';
+ $TWILIO_API_SECRET = 'MFjIWKTtNuNHAmRkeggIViDJplBQxyUW';
